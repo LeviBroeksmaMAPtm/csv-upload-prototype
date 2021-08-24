6310f839dcc2ab993e130de9e54da771bda5e142
Martijn Harmenzon <martijn.harmenzon@maptm.nl>
2021-08-24 16:37:49 +0200

    Add feature groups to map01

--- frontend/src/components/twee/alpine_func_twee.js
+++ frontend/src/components/twee/alpine_func_twee.js
@@ -120,7 +120,7 @@ window.csvParser = () => {
 
           filtered.map(filterRow => {
             if (filterRow.longitude != "" && filterRow.latitude != "" && filterRow.modaliteit != "") {
-              lonlat = { longitude: filterRow.longitude, latitude: filterRow.latitude, modaliteit: filterRow.modaliteit };
+              const lonlat = { longitude: filterRow.longitude, latitude: filterRow.latitude, modaliteit: filterRow.modaliteit };
               geoArr.push(lonlat);
               const button = document.getElementById('confirmation-btn');
               button.disabled = false;
@@ -135,34 +135,37 @@ window.csvParser = () => {
             const coordinates = [geoArrRow.longitude, geoArrRow.latitude];
             const modaliteit = geoArrRow.modaliteit;
 
-
             if (modaliteit === 'fiets') {
-              const lonlatToAdd = L.circleMarker(coordinates, {
+              L.circleMarker(coordinates, {
                 color: 'gray',
                 fillColor: 'gray',
                 fillOpacity: 0.5,
                 radius: 50
-              }).bindPopup('Telpunt: ' + modaliteit);
-              const lg_bike = L.layerGroup([lonlatToAdd]).addTo(mymap);
+              })
+                .bindPopup('Telpunt: ' + modaliteit)
+                .addTo(this.fg_bike);
             }
+
             if (modaliteit === 'auto') {
-              const lonlatToAdd = L.circleMarker(coordinates, {
+              L.circleMarker(coordinates, {
                 color: 'blue',
                 fillColor: 'blue',
                 fillOpacity: 0.5,
                 radius: 50
-              }).bindPopup('Telpunt: ' + modaliteit);
-              const lg_car = L.layerGroup([lonlatToAdd]).addTo(mymap);
+              })
+                .bindPopup('Telpunt: ' + modaliteit)
+                .addTo(this.fg_car);
             }
+
             if (modaliteit === 'voetganger') {
-              const lonlatToAdd = L.circle(coordinates, {
+              L.circle(coordinates, {
                 color: 'red',
                 fillColor: 'red',
                 fillOpacity: 0.5,
                 radius: 50
-              }).bindPopup('Telpunt: ' + modaliteit);
-              const lg_walk = L.layerGroup([lonlatToAdd]).addTo(mymap);
-
+              })
+                .bindPopup('Telpunt: ' + modaliteit)
+                .addTo(this.fg_walk);
             }
           }); // End geoArr.map()
         } // End complete: property
